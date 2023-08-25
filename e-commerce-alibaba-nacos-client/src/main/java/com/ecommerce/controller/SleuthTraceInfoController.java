2010b33c49a122e7f7e9feddc561adda7f3dc697
LiYiFeng <4471727473@qq.com>
2023-08-25 16:02:32 +0800

    打印跟踪信息

---
+++ e-commerce-alibaba-nacos-client/src/main/java/com/ecommerce/controller/SleuthTraceInfoController.java
@@ -0,0 +1,14 @@
+package com.ecommerce.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 打印跟踪信息
+ */
+@Slf4j
+@RestController
+@RequestMapping("/sleuth")
+public class SleuthTraceInfoController {
+}
