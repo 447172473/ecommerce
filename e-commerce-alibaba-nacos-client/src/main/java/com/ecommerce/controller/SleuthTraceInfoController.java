4825f067f11c29b2c7fc31b8e9ba7d988bf92607
LiYiFeng <4471727473@qq.com>
2023-08-25 16:41:45 +0800

    打印跟踪信息 service

--- e-commerce-alibaba-nacos-client/src/main/java/com/ecommerce/controller/SleuthTraceInfoController.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/ecommerce/controller/SleuthTraceInfoController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.service.SleuthTraceInfoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sleuth")
 public class SleuthTraceInfoController {
+
+    private final SleuthTraceInfoService traceInfoService;
+
+    public SleuthTraceInfoController(SleuthTraceInfoService traceInfoService) {
+        this.traceInfoService = traceInfoService;
+    }
+
+    /**
+     * <h2>打印日志跟踪信息</h2>
+     * */
+    @GetMapping("/trace-info")
+    public void logCurrentTraceInfo() {
+        traceInfoService.logCurrentTraceInfo();
+    }
 }
